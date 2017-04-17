fca63df9048c525f8c08028bb10fd0a757f1560a
Tomorovik <tomorovik@gmail.com>
2017-04-17 12:28:56 +0200

    Added model for BookFile

---
+++ BookManager/BookManager/Model/BookFile.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Text;
+
+namespace BookManager.Model
+{
+    public class BookFile
+    {
+        public string FileName { get; set; }
+        public string FileLocation { get; set; }
+        public byte[] Hash { get; set; }
+        public string PublicHash => Encoding.Default.GetString(Hash);
+        public string ShortFileName => Path.GetFileName(FileName);
+        public string Extension => Path.GetExtension(FileName);
+    }
+}
