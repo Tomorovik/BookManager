9fac2c6771a6e7119def2fe463d759ab9f886f12
Tomorovik <tomorovik@gmail.com>
2017-04-17 14:39:26 +0200

    Created custom window for input

---
+++ BookManager/BookManager/View/CustomView/InputDialog.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+
+namespace BookManager.View.CustomView
+{
+    /// <summary>
+    /// Interaction logic for InputDialog.xaml
+    /// </summary>
+    public partial class InputDialog : Window
+    {
+        public InputDialog()
+        {
+            InitializeComponent();
+        }
+        public InputDialog(string defaultAnswer = "")
+        {
+            InitializeComponent();
+            fileName.Text = defaultAnswer;
+        }
+
+        private void DialogOk_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            fileName.SelectAll();
+            fileName.Focus();
+        }
+
+        public string Input
+        {
+            get { return fileName.Text; }
+        }
+    }
+}
+}
