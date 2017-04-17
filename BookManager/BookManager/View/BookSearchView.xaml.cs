0d3d5b28d753f944a96b8d2cb9b9aa637902cdb4
Tomorovik <tomorovik@gmail.com>
2017-04-17 13:30:16 +0200

    Made changes for BookSearchView for using adequate viewmodel.

--- BookManager/BookManager/View/BookSearchView.xaml.cs
+++ BookManager/BookManager/View/BookSearchView.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using BookManager.ViewModel;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace BookManager.View
 {
@@ -22,6 +10,7 @@ namespace BookManager.View
     {
         public BookSearchView()
         {
+            this.DataContext = new BookSearchViewModel();
             InitializeComponent();
         }
     }
